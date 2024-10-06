dc3cb50e25e2a3588f65b2f129d5726d4bdc0b70
edersonbadeca <edersonbadeca@gmail.com>
2024-10-06 15:02:36 -0300

    feat: create supplier service

---
+++ service/supplier_service.py
@@ -0,0 +1,31 @@
+from repository.repository import Repository
+from models.models import Supplier
+
+class SupplierService:
+    def __init__(self, repository: Repository):
+        self.repository = repository
+
+    def create_supplier(self, name: str, contact_info: str, address: str):
+        new_supplier = Supplier(name=name, contact_info=contact_info, address=address)
+        self.repository.add_supplier(new_supplier)
+        return new_supplier
+
+    def fetch_supplier(self, supplier_id: int):
+        return self.repository.fetch_supplier_by_id(supplier_id)
+
+    def update_supplier(self, supplier_id: int, name: str, contact_info: str, address: str):
+        supplier = self.repository.fetch_supplier_by_id(supplier_id)
+        if supplier:
+            supplier.name = name
+            supplier.contact_info = contact_info
+            supplier.address = address
+            self.repository.update_supplier(supplier)
+            return supplier
+        return None
+
+    def delete_supplier(self, supplier_id: int):
+        supplier = self.repository.fetch_supplier_by_id(supplier_id)
+        if supplier:
+            self.repository.delete_supplier(supplier)
+            return True
+        return False
