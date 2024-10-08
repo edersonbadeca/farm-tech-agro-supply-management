35710284e164dfeccd0ac5beebc5fc7228d2ef91
edersonbadeca <edersonbadeca@gmail.com>
2024-10-08 00:13:36 -0300

    feat: update initial mer

--- scripts/create-tables.sql
+++ scripts/create-tables.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS suppliers (
+CREATE TABLE IF NOT EXISTS APP.suppliers (
     id INTEGER PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     contact_info VARCHAR(100),
     address VARCHAR(255)
 );
 
-CREATE TABLE IF NOT EXISTS inputs (
+CREATE TABLE IF NOT EXISTS APP.inputs (
     id INTEGER PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     category VARCHAR(50) NOT NULL,
@@ -13,14 +13,18 @@ CREATE TABLE IF NOT EXISTS inputs (
     expiration_date DATE NOT NULL,
     supplier_id INTEGER,
     addition_date DATE NOT NULL,
-    FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
+    FOREIGN KEY (supplier_id) REFERENCES APP.suppliers(id)
 );
 
-CREATE TABLE IF NOT EXISTS stock_movements (
+CREATE TABLE IF NOT EXISTS APP.stock_movements (
     id INTEGER PRIMARY KEY,
     input_id INTEGER,
     quantity INTEGER NOT NULL,
-    movement_type VARCHAR(10) NOT NULL,  -- 'in' for stock additions, 'out' for stock deductions
+    movement_type VARCHAR(10) NOT NULL,
     movement_date DATE NOT NULL,
-    FOREIGN KEY (input_id) REFERENCES inputs(id)
+    FOREIGN KEY (input_id) REFERENCES APP.inputs(id)
 );
+CREATE USER APP IDENTIFIED BY 123456;
+
+ALTER USER APP QUOTA UNLIMITED ON USERS;
+ALTER USER APP QUOTA 500M ON USERS;
